f35ddcc823ecc75083cc9c51a4011f8c5813ed0b
AxelUrizar <axelurizarmartin@gmail.com>
2023-12-25 11:45:06 +0100

    Minor UI change (vh to dvh on layout).

--- client/src/component/Layout.jsx
+++ client/src/component/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 export function Layout() {
   return (
-    <div className="w-full h-screen bg-zinc-900 text-white py-3 px-8 flex flex-col items-center">
+    <div className="w-full h-[100dvh] bg-zinc-900 text-white py-3 px-8 flex flex-col items-center">
       <Outlet />
     </div>
   )
